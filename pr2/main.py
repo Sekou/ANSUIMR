64bc3dca3f09e7b0f2cbe2461314b57be99ac55e
Sekou <sekoudiane1990@gmail.com>
2024-09-27 20:01:51 +0300

    pr2, car on a track

--- pr2/main.py
+++ pr2/main.py
@@ -41,10 +41,11 @@ class Obstacle:
     def __init__(self, x, y):
         self.x=x
         self.y=y
-        self.R=30
+        self.R=20
     def getPos(self):
         return [self.x, self.y]
     def draw(self, screen):
+        pygame.draw.circle(screen, (200,200,200), self.getPos(), self.R, 0)
         pygame.draw.circle(screen, (0,0,0), self.getPos(), self.R, 2)
 
 class Robot:
@@ -56,11 +57,17 @@ class Robot:
         self.W=40
         self.speed=0
         self.steer=0
-        self.traj=[]
+        self.traj=[] #точки траектории
+        self.vals1=[] #значения пользователя 1
+        self.vals2=[] #значения пользователя 2
         self.A=3 #стремление к цели
         self.B=2 #боязнь столкновений
     def getPos(self):
         return [self.x, self.y]
+    def clear(self):
+        self.traj = []
+        self.vals1 = []
+        self.vals2 = []
 
     def draw(self, screen):
         p=np.array(self.getPos())
@@ -77,6 +84,7 @@ class Robot:
         for i in range(len(self.traj)-1):
             pygame.draw.line(screen, (0,0,255), self.traj[i], self.traj[i+1], 1)
     def sim(self, dt):
+        self.addedTrajPt = False
         delta=[self.speed*dt, 0]
         delta=rot(delta, self.alpha)
         self.x+=delta[0]
@@ -89,6 +97,7 @@ class Robot:
         p=self.getPos()
         if len(self.traj)==0 or dist(p, self.traj[-1])>10:
             self.traj.append(self.getPos())
+            self.addedTrajPt=True
 
     def goto(self, pos, obsts, dt):
         #1
@@ -103,27 +112,49 @@ class Robot:
         da2 = limAng(aObst - self.alpha)
         #3
         self.steer += (self.A*da-self.B*da2) * dt
-        if self.steer > 1: self.steer = 1
-        if self.steer < -1: self.steer = -1
+        maxSteer=1
+        if self.steer > maxSteer: self.steer = maxSteer
+        if self.steer < -maxSteer: self.steer = -maxSteer
         self.speed = 50
 
+class Road:
+    def __init__(self, R, W, x, y):
+        self.x=x
+        self.y=y
+        self.R=R
+        self.W=W
+    def getPos(self):
+        return [self.x, self.y]
+    def draw(self, screen):
+        pygame.draw.circle(screen, (0,0,0), self.getPos(), self.R+self.W/2, 2)
+        pygame.draw.circle(screen, (150,150,150), self.getPos(), self.R, 1)
+        pygame.draw.circle(screen, (0,0,0), self.getPos(), self.R-self.W/2, 2)
+
 def main():
+    with open('log.txt', 'w') as f: f.write("")
+
     screen = pygame.display.set_mode(sz)
     timer = pygame.time.Clock()
     fps = 20
 
-    r=Robot(100,100,1)
-    with open('log.txt', 'w') as f: f.write("")
+    road=Road(220, 150, 400, 300)
+    robot=Robot(100, 100, 1)
 
     AA=[5, 7, 10] #варианты коэфф-та движения к цели
     BB=[5, 7, 10] #варианты коэфф-та отталкивания от препятствий
     variants=list(itertools.product(AA, BB))
-
     indVariant=0
 
-    obsts=[Obstacle(200,200), Obstacle(400,300), Obstacle(270,350), Obstacle(250,450)]
+    obsts=[]
+    nObst=10
 
-    goal=[500,500]
+    def rn(A):
+        return 2*(0.5-np.random.rand())*A
+    for i in range(nObst):
+        a=2*math.pi/nObst * i
+        radius=road.R+rn(road.W/3)
+        p = [road.x + radius * math.cos(a), road.y + radius * math.sin(a)]
+        obsts.append(Obstacle(*p))
 
     time=0
 
@@ -131,44 +162,59 @@ def main():
         for ev in pygame.event.get():
             if ev.type==pygame.QUIT:
                 sys.exit(0)
-            if ev.type == pygame.KEYDOWN:
-                if ev.key == pygame.K_r:
-                    print("Hi")
-
         dt=1/fps
-
         screen.fill((255, 255, 255))
 
-        r.A, r.B = variants[indVariant]
-        r.goto(goal, obsts, dt)
-        r.sim(dt)
-        r.draw(screen)
+        ang=-time*0.15+math.pi
+        goal = [road.x+road.R*math.cos(ang), road.y+road.R*math.sin(ang)]
 
+        robot.A, robot.B = variants[indVariant]
+        robot.goto(goal, obsts, dt)
+
+        robot.sim(dt)
+        if robot.addedTrajPt:
+            #ошибка до цели
+            robot.vals1.append(dist(robot.getPos(), goal))
+            #ошибка до трассы
+            robot.vals2.append(abs(dist(road.getPos(), robot.getPos()) - road.R))
+
+        road.draw(screen)
         for o in obsts:
             o.draw(screen)
+        robot.draw(screen)
 
         pygame.draw.circle(screen, (255,0,0), goal, 5, 2)
 
+        L = 0 #длина пути
+        for i in range(len(robot.traj) - 1):
+            L += dist(robot.traj[i], robot.traj[i + 1])
+        N = 0 #число столкновений
+        for i in range(len(robot.traj)):
+            d = min(dist(robot.traj[i], o.getPos()) for o in obsts)
+            if d < 50: N += 1
+        M = len(obsts)
+        D=np.mean(robot.vals1)
+        E=np.mean(robot.vals2)
+
+        Q=10000/(E*(1+N)*D)
+
         drawText(screen, f"Time = {time:.3f}", 5, 5)
         drawText(screen, f"Variant = {indVariant+1}/{len(variants)}", 5, 25)
-        drawText(screen, f"A {r.A}; B {r.B}", 5, 45)
+        drawText(screen, f"A {robot.A}; B {robot.B}", 5, 45)
+        drawText(screen, f"M = {M}", 5, 65)
+        drawText(screen, f"L = {L:.1f}", 5, 85)
+        drawText(screen, f"N = {N}", 5, 105)
+        drawText(screen, f"D = {D:.1f}", 5, 125)
+        drawText(screen, f"E = {E:.3f}", 5, 145)
+        drawText(screen, f"Q = {Q:.3f}", 5, 165)
 
-        if dist(r.getPos(), goal)<50 or time>20:
-
-            L=0
-            for i in range(len(r.traj)-1):
-                L+=dist(r.traj[i], r.traj[i+1])
-            N=0
-            for i in range(len(r.traj)):
-                d=min(dist(r.traj[i], o.getPos()) for o in obsts)
-                if d<50: N+=1
-            M=len(obsts)
+        if time>30:
             with open('log.txt', 'a') as f:
-                f.write(f"{indVariant}; {r.A}; {r.B}; {time:.3f}; {M}; {L}; {N}; {str(r.traj)}\n")
+                f.write(f"{indVariant}; {robot.A}; {robot.B}; {time:.3f}; {M}; {L}; {N}; {D}; {E}; {Q}; {str(robot.traj)}\n")
                 print(f"Tested indVariant: {indVariant}")
             indVariant+=1
-            r.traj=[]
-            r = Robot(100, 100, 1)
+            robot.clear()
+            robot = Robot(100, 100, 1)
             time=-dt #zero
             if indVariant>=len(variants):
                 break
