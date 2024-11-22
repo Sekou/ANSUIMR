4fb45ef62c62b6cf8d1716edc5d3ddf9fa59d7ba
Sekou <sekoudiane1990@gmail.com>
2024-11-22 15:50:20 +0300

    pr11 - wolfs sheeps grass - added code

--- pr11/main.py
+++ pr11/main.py
@@ -4,7 +4,15 @@ import numpy as np
 import math
 
 fps = 20
-GRASS_REGROWTH_TIME=60/fps
+NUM_PATCHES_X=20
+NUM_PATCHES_Y=20
+INITIAL_NUMBER_SHEEP=20
+INITIAL_NUMBER_WOLVES=5
+GRASS_REGROWTH_TIME=70
+SHEEP_GAIN_FROM_FOOD=60
+WOLF_GAIN_FROM_FOOD=40
+SHEEP_REPRODUCE=4
+WOLF_REPRODUCE=20
 
 pygame.font.init()
 def drawText(screen, s, x, y, sz=20, color=(0,0,0)): #отрисовка текста
@@ -61,16 +69,18 @@ class Cell:
         if self.countdown>GRASS_REGROWTH_TIME:
             self.hasGrass=True
         else:
-            self.countdown += dt
+            self.countdown +=1
             self.hasGrass=False
 
 class Agent:
-    def __init__(self, x, y, type, sz):
+    def __init__(self, x, y, type, sz, energy):
         self.x, self.y, self.type, self.sz = x, y, type, sz
         self.color = (255, 255, 255) if type=="sheep" else (0,0,0)
         self.targetPos=None
         self.targetCell=None
-        self.targetAgent=None
+        self.targetSheep=None
+        self.removed=False
+        self.energy=energy
     def getPos(self):
         return [self.x, self.y]
     def draw(self, screen):
@@ -91,9 +101,30 @@ class Agent:
             return cc[np.argmin(dd)]
         else:
             return None
-    def sim(self, dt, cells):
-        if self.targetCell is None:
+    def getNearestSheep(self, agents):
+        aa=[a for a in agents if a.type=="sheep"]
+        dd=[dist(self.getPos(), a.getPos()) for a in aa]
+        if len(dd)>0:
+            return aa[np.argmin(dd)]
+        else:
+            return None
+    def tryReproduce(self, dt, agents):
+        if self.targetCell is None and self.type=="sheep":
+            if np.random.random()<SHEEP_REPRODUCE/100:
+                self.energy/=2
+                agents.append(Agent(self.x+self.sz, self.y, "sheep", self.sz, self.energy))
+        if self.targetSheep is None and self.type=="wolf":
+            if np.random.random()<WOLF_REPRODUCE/100:
+                self.energy/=2
+                agents.append(Agent(self.x+self.sz, self.y, "wolf", self.sz, self.energy))
+    def sim(self, dt, cells, agents):
+        self.energy-=1
+        if self.energy==0: return
+        self.tryReproduce(dt, agents)
+        if self.targetCell is None and self.type=="sheep":
             self.targetCell=self.getNearestGrass(cells)
+        if self.targetSheep is None and self.type=="wolf":
+            self.targetSheep=self.getNearestSheep(agents)
         if self.targetCell is not None:
             self.targetPos=self.targetCell.getPos()
             v=np.subtract(self.targetPos, self.getPos())
@@ -105,6 +136,20 @@ class Agent:
                 self.targetCell.hasGrass=False
                 self.targetCell.countdown=0
                 self.targetCell=None
+                self.energy+=SHEEP_GAIN_FROM_FOOD
+        if self.targetSheep is not None:
+            self.targetSheep=self.getNearestSheep(agents)
+            if self.targetSheep is not None:
+                self.targetPos = self.targetSheep.getPos()
+                v = np.subtract(self.targetPos, self.getPos())
+                v = 70 * v / np.linalg.norm(v) * dt
+                self.x += v[0]
+                self.y += v[1]
+                d = dist(self.getPos(), self.targetPos)
+                if d < self.sz:
+                    self.targetSheep.removed = True
+                    self.targetSheep = None
+                    self.energy += WOLF_GAIN_FROM_FOOD
 
 class Grid:
     def __init__(self, x0, y0, sz, nx, ny):
@@ -131,13 +176,19 @@ def main():
     timer = pygame.time.Clock()
 
     # cell = Cell(200, 200, 30)
-    grid = Grid(200,200,30,10,10)
+    grid = Grid(100,25,25,NUM_PATCHES_X,NUM_PATCHES_Y)
 
     agents=[]
-    for i in range(10):
+    for i in range(INITIAL_NUMBER_SHEEP):
         x=rnd(grid.x0, grid.x0+grid.sz*grid.nx)
         y=rnd(grid.y0, grid.y0+grid.sz*grid.ny)
-        a=Agent(x,y,"sheep",30)
+        a=Agent(x,y,"sheep",30, rnd(0, 2*SHEEP_GAIN_FROM_FOOD))
+        agents.append(a)
+
+    for i in range(INITIAL_NUMBER_WOLVES):
+        x=rnd(grid.x0, grid.x0+grid.sz*grid.nx)
+        y=rnd(grid.y0, grid.y0+grid.sz*grid.ny)
+        a=Agent(x,y,"wolf",30, rnd(0, 2*WOLF_GAIN_FROM_FOOD))
         agents.append(a)
 
     for c in grid.getAllCells():
@@ -156,7 +207,10 @@ def main():
         grid.sim(dt)
 
         for a in agents:
-            a.sim(dt, grid.getAllCells())
+            a.sim(dt, grid.getAllCells(), agents)
+
+
+        agents=[a for a in agents if not a.removed and a.energy>0]
 
         screen.fill((255, 255, 255))
         grid.draw(screen)
